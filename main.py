921ebf692f727b72ff62c2711a747aacbd5e7db8
Kevin Ammersinn <kevin@ammersinn.ch>
2024-08-22 13:32:14 +0200

    add class

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-# This file is to train OOP
+class Language:
+    def __init__(self, name=""):
+        self._name = name
+    
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    def __str__(self):
+        return f"{self.name} is a programming language!"
+
+
 def main():
-    print("Hello World!")
+    python = Language("Python")
+    print(python)
 
 
 if __name__ == "__main__":
